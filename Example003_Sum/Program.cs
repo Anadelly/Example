e4484aee5e8180d35b8d80c3a81d14ef10c9606e
Anadell <lemeshkoav@yandex.ru>
2023-10-05 11:08:59 +0300

    add Random to 003_Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,9 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿//int numberA = 3;
+int numberA = new Random().Next(1,10); // 1...9
+System.Console.WriteLine(numberA);
+//int numberB = 5;
+int numberB = new Random().Next(1,10);
+System.Console.WriteLine(numberB);
 int result = numberA + numberB;
 System.Console.WriteLine(result);
 
