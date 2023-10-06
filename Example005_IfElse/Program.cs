3ebccdb7a21a82334e61249fae253be3f55fc61c
Anadell <lemeshkoav@yandex.ru>
2023-10-06 18:05:38 +0300

    add Example005_IfElse, не читает русский

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+System.Console.WriteLine(username);
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
